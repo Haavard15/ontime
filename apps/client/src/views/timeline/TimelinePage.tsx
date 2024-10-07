a9b28acd7dd4bd2cda3078efc01ac3d00c8dc81d
Carlos Valente <carlosvalente@pm.me>
2024-10-07 12:26:28 +0200

    chore: extract views directory

--- apps/client/src/views/timeline/TimelinePage.tsx
+++ apps/client/src/views/timeline/TimelinePage.tsx
@@ -1,12 +1,12 @@
 import { useMemo } from 'react';
 import { MaybeString, OntimeEvent, ProjectData, Settings } from 'ontime-types';
 
-import ViewParamsEditor from '../../../common/components/view-params-editor/ViewParamsEditor';
-import { useWindowTitle } from '../../../common/hooks/useWindowTitle';
-import { ViewExtendedTimer } from '../../../common/models/TimeManager.type';
-import { formatTime, getDefaultFormat } from '../../../common/utils/time';
-import { useTranslation } from '../../../translation/TranslationProvider';
-import SuperscriptTime from '../common/superscript-time/SuperscriptTime';
+import ViewParamsEditor from '../../common/components/view-params-editor/ViewParamsEditor';
+import { useWindowTitle } from '../../common/hooks/useWindowTitle';
+import { ViewExtendedTimer } from '../../common/models/TimeManager.type';
+import { formatTime, getDefaultFormat } from '../../common/utils/time';
+import SuperscriptTime from '../../features/viewers/common/superscript-time/SuperscriptTime';
+import { useTranslation } from '../../translation/TranslationProvider';
 
 import Section from './timeline-section/TimelineSection';
 import Timeline from './Timeline';
